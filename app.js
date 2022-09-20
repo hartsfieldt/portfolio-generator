60feb61ef0f58f410cddffd2c25fdfd3de118bf0
Your Name <you@example.com>
2022-09-19 22:29:30 -0400

    refactored code

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = (profileDataArr) => {
   for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -10,3 +9,4 @@ const printProfileData = (profileDataArr) => {
   profileDataArr.forEach((profileItem) => console.log(profileItem));
 };
 
+printProfileData(profileDataArgs);
