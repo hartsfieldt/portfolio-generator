59270efd251dd38d99905f5cdcf94c3d0a5bbdd3
Your Name <you@example.com>
2022-09-22 21:11:09 -0400

    updated app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require("fs");
 
-const [name, github] = profileDataArgs;
+// const generatePage = require("./src/page-template.js");
 
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile("./index.html", pageHTML, (err) => {
-  if (err) throw err;
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
