2bfe3f980463b015b7b1440f18a901e52a3d4798
Your Name <you@example.com>
2022-09-25 10:52:31 -0400

    updated validate function

--- app.js
+++ app.js
@@ -19,11 +19,33 @@ const promptUser = () => {
       type: "input",
       name: "github",
       message: "Enter your GitHub Username",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        "Would you like to enter some information about yourself for an 'About' section?",
+      default: true,
     },
     {
       type: "input",
       name: "about",
-      message: "Provide some information about yourself:",
+      message: "Provide some information about yourself.",
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      },
     },
   ]);
 };
@@ -49,6 +71,14 @@ const promptProject = (portfolioData) => {
         type: "input",
         name: "description",
         message: "Provide a description of the project (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your name!");
+            return false;
+          }
+        },
       },
       {
         type: "checkbox",
@@ -68,6 +98,14 @@ const promptProject = (portfolioData) => {
         type: "input",
         name: "link",
         message: "Enter the GibHub link to your project. (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your name!");
+            return false;
+          }
+        },
       },
       {
         type: "confirm",
